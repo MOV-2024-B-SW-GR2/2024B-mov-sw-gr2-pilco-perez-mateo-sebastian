7f34881d880241e9afa7151a0a850a27754c48e9
SebasPM15 <94476050+MateoPm15@users.noreply.github.com>
2025-02-01 13:19:25 -0500

    Clase 30/01 and Fix GGoogleMaps

--- 01-Android/app/src/main/java/com/example/gr2sw2024b_mspp/GGoogleMaps.kt
+++ 01-Android/app/src/main/java/com/example/gr2sw2024b_mspp/GGoogleMaps.kt
@@ -21,12 +21,10 @@ import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.snackbar.Snackbar
 
 class GGoogleMaps : AppCompatActivity() {
-
     private lateinit var mapa: GoogleMap
     var permisos = false
     val nombrePermisoFine = android.Manifest.permission.ACCESS_FINE_LOCATION
     val nombrePermisoCoarse = android.Manifest.permission.ACCESS_COARSE_LOCATION
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,91 +35,80 @@ class GGoogleMaps : AppCompatActivity() {
             insets
         }
         solicitarPermisos()
-        //Inicializar lógica del mapa
         inicializarLogicaMapa()
-
-        val btn_Carolina = findViewById<Button>(R.id.btn_ir_carolina)
-        btn_Carolina.setOnClickListener {
-            val carolina = LatLng(-0.1838874666425376, -78.48462687495609)
+        val botonCarolina = findViewById<Button>(R.id.btn_ir_carolina)
+        botonCarolina.setOnClickListener {
+            val carolina = LatLng(-0.18221288005854652, -78.48553955554578)
             moverCamaraConZoom(carolina)
         }
-
     }
-
-    fun tengoPermisos(): Boolean {
+    fun tengoPermisos():Boolean{
         val contexto = applicationContext
         val permisoFine = ContextCompat.checkSelfPermission(contexto, nombrePermisoFine)
         val permisoCoarse = ContextCompat.checkSelfPermission(contexto, nombrePermisoCoarse)
         val tienePermisos = permisoFine == PackageManager.PERMISSION_GRANTED &&
                 permisoCoarse == PackageManager.PERMISSION_GRANTED
-            permisos = tienePermisos
+        permisos = tienePermisos
         return permisos
     }
-
-    fun solicitarPermisos() {
-        if(!tengoPermisos()) {
+    fun solicitarPermisos(){
+        if(!tengoPermisos()){
             ActivityCompat.requestPermissions(
                 this, arrayOf(nombrePermisoFine, nombrePermisoCoarse), 1
             )
         }
     }
 
-    fun inicializarLogicaMapa () {
+    fun inicializarLogicaMapa(){
         val fragmentoMapa = supportFragmentManager.findFragmentById(
-            R.id.map
-        ) as SupportMapFragment
+            R.id.map ) as SupportMapFragment
         fragmentoMapa.getMapAsync { googleMap ->
-            with(googleMap) {
+            with(googleMap){
                 mapa = googleMap
                 establecerConfiguracionMapa()
                 moverQuicentro()
-                anadirPoliLinea()
+                anadirPolilinea()
                 anadirPoligono()
                 escucharListeners()
             }
         }
     }
-
-    fun moverQuicentro() {
-        val quicentro = LatLng(-0.17604982296243202, -78.47915825407831)
-        val titulo = "Quicentro Norte"
+    fun moverQuicentro(){
+        val quicentro = LatLng(-0.17633018352832922, -78.47853222234333)
+        val titulo = "Quicentro"
         val marcadorQuicentro = anadirMarcador(quicentro, titulo)
         marcadorQuicentro.tag = titulo
         moverCamaraConZoom(quicentro)
     }
-
-    fun anadirPoliLinea (){
-        with(mapa) {
+    fun anadirPolilinea(){
+        with(mapa){
             val polilinea = mapa.addPolyline(
                 PolylineOptions()
                     .clickable(true)
                     .add(
-                        LatLng(-0.17604982296243202, -78.47915825407831),
-                        LatLng(-0.21205698072778867, -78.49033537723614),
-                        LatLng(-0.2521180963744332, -78.52284538887682)
+                        LatLng(-0.17791267925471754, -78.48185816127831),
+                        LatLng(-0.18019791013168018, -78.48539867691878),
+                        LatLng(-0.1822149211841061, -78.48320999452285)
                     )
             )
-            polilinea.tag = "polilinea - 1"
+            polilinea.tag = "polilinea-uno"
         }
     }
-
-    fun anadirPoligono () {
-        with(mapa) {
+    fun anadirPoligono(){
+        with(mapa){
             val poligono = mapa.addPolygon(
                 PolygonOptions()
                     .clickable(true)
                     .add(
-                        LatLng(-0.23337524391110015, -78.49576412171903),
-                        LatLng(-0.23327868518910438, -78.49367199871227),
-                        LatLng(-0.23551026437207337, -78.49395094844651)
+                        LatLng(-0.17810579736798682, -78.48025956482248),
+                        LatLng(-0.18047685848208925, -78.47937980033),
+                        LatLng(-0.17664668268438008, -78.4796694788824),
                     )
             )
-            poligono.tag = "poligono - 1"
-
+            poligono.tag = "poligono-uno"
         }
     }
-
-    fun escucharListeners() {
+    fun escucharListeners(){
         mapa.setOnPolygonClickListener {
             mostrarSnackbar("setOnPolygonClickListener ${it.tag}")
         }
@@ -138,31 +125,28 @@ class GGoogleMaps : AppCompatActivity() {
     }
 
     @SuppressLint("MissingPermission")
-    fun establecerConfiguracionMapa () {
-        with(mapa) {
-            if (tengoPermisos()) {
+    fun establecerConfiguracionMapa(){
+        with(mapa){
+            if(tengoPermisos()){
                 mapa.isMyLocationEnabled = true
                 uiSettings.isMyLocationButtonEnabled = true
             }
             uiSettings.isZoomControlsEnabled = true
         }
     }
-
-    fun moverCamaraConZoom(latLang: LatLng, zoom: Float = 17f) {
-        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(latLang, zoom))
+    fun moverCamaraConZoom(latLang:LatLng, zoom: Float = 17f){
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(latLang,zoom))
     }
-
-    fun anadirMarcador(latLang: LatLng, title: String):Marker {
+    fun anadirMarcador(latLang:LatLng, title: String):Marker{
         return mapa.addMarker(MarkerOptions().position(latLang).title(title))!!
     }
-
-    fun mostrarSnackbar(texto: String) {
-        var snack = Snackbar.make(
+    fun mostrarSnackbar(texto: String){
+        val snack = Snackbar.make(
             findViewById(R.id.main),
             texto,
             Snackbar.LENGTH_INDEFINITE
         )
         snack.show()
     }
-
 }
+
